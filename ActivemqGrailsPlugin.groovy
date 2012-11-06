d4ebedbef99434652b95ac593421000f9ec7c5be
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-11-06 11:10:17 -0600

    removed unused code

--- ActivemqGrailsPlugin.groovy
+++ ActivemqGrailsPlugin.groovy
@@ -60,29 +60,5 @@ class ActivemqGrailsPlugin {
     defaultJmsTemplate(org.springframework.jms.core.JmsTemplate) {
       connectionFactory = ref("jmsConnectionFactory")
     }
-
-  }
-
-  def doWithApplicationContext = { applicationContext ->
-    // TODO Implement post initialization spring config (optional)
-  }
-
-  def doWithWebDescriptor = { xml ->
-    // TODO Implement additions to web.xml (optional)
-  }
-
-  def doWithDynamicMethods = { ctx ->
-    // TODO Implement registering dynamic methods to classes (optional)
-  }
-
-  def onChange = { event ->
-    // TODO Implement code that is executed when any artefact that this plugin is
-    // watching is modified and reloaded. The event contains: event.source,
-    // event.application, event.manager, event.ctx, and event.plugin.
-  }
-
-  def onConfigChange = { event ->
-    // TODO Implement code that is executed when the project configuration changes.
-    // The event is the same as for 'onChange'.
   }
 }
